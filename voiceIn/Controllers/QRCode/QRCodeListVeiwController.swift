b6ce56a93687696fcd63bfb3e9a2c5e1d001a8cc
lockys <locky4567@gmail.com>
2016-05-30 01:49:48 +0800

    improve the overlay indicator.

--- voiceIn/Controllers/QRCode/QRCodeListVeiwController.swift
+++ voiceIn/Controllers/QRCode/QRCodeListVeiwController.swift
@@ -104,7 +104,7 @@ class QRCodeListVeiwController: UITableViewController {
         let getInformationApiRoute = API_END_POINT + "/accounts/" + UserPref.getUserPrefByKey("userUuid") + "/customQrcodes"
         
         self.tableView.reloadData()
-        SwiftOverlays.showCenteredWaitOverlayWithText(self.tableView!, text: "讀取中...")
+        SwiftOverlays.showCenteredWaitOverlayWithText(self.view.superview!, text: "讀取中...")
         self.view.userInteractionEnabled = false
         
         Alamofire
@@ -134,7 +134,10 @@ class QRCodeListVeiwController: UITableViewController {
                     AlertBox.createAlertView(self, title: "抱歉..", body: "可能為網路或伺服器錯誤，請等一下再試", buttonValue: "確認")
                 }
                 
-                SwiftOverlays.removeAllOverlaysFromView(self.tableView!)
+                if let superview = self.view.superview {
+                    SwiftOverlays.removeAllOverlaysFromView(superview)
+                }
+                
                 self.view.userInteractionEnabled = true
                 self.refreshControl?.endRefreshing()
         }
